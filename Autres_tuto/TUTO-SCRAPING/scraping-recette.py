938302fd0127ad65e7b4f744bae15e0b6e29d4b6
MSInTech <mschenevier@gmail.com>
2024-08-31 22:50:29 +0200

    def

--- Autres_tuto/TUTO-SCRAPING/scraping-recette.py
+++ Autres_tuto/TUTO-SCRAPING/scraping-recette.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 
 url = "https://www.galaxus.ch/en"
 
+def get_text_if_not_none(e):
+    if e:
+        return e.text
+    return None
+
 response = requests.get(url)
 
 if response.status_code == 200:
@@ -15,9 +20,11 @@ if response.status_code == 200:
 
     soup = BeautifulSoup(html, "html5lib")
 
-    titre = soup.find("h4")
-
+    titre = soup.find("h2", class_="sc-f93c62dd-1 emlFda").text
     print(titre)
+
+    description = get_text_if_not_none(soup.find("p", class_="sc-2e43a11b-3 ezoffT").text)
+    print(description)
 
 else:
     print("ERREUR", response.status_code)
