97798f4ef7c48d53f0462044eead071e2672f261
MSInTech <mschenevier@gmail.com>
2024-08-31 22:12:18 +0200

    updtae

--- Autres_tuto/TUTO-SCRAPING/scraping-recette.py
+++ Autres_tuto/TUTO-SCRAPING/scraping-recette.py
@@ -1,4 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
+
 url = "https://www.marmiton.org/"
-response = requests.get()
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    # print(html)
+
+    f = open("recette.html", "w")
+    f.write(html)
+    f.close()
+
+    soup = BeautifulSoup(html, "html5lib")
+
+else:
+    print("ERREUR", response.status_code)
+
+
 print("FIN")
