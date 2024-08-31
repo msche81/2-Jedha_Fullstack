3c0abff204fe0d00e671d935994054647ddc5bfa
MSInTech <mschenevier@gmail.com>
2024-08-31 23:17:36 +0200

    updtae

--- Autres_tuto/TUTO-SCRAPING/scraping-recette.py
+++ Autres_tuto/TUTO-SCRAPING/scraping-recette.py
@@ -5,7 +5,7 @@ url = "https://www.galaxus.ch/en"
 
 def get_text_if_not_none(e):
     if e:
-        return e.text
+        return e.text.strip()
     return None
 
 response = requests.get(url)
@@ -23,8 +23,14 @@ if response.status_code == 200:
     titre = soup.find("h2", class_="sc-f93c62dd-1 emlFda").text
     print(titre)
 
-    description = get_text_if_not_none(soup.find("p", class_="sc-2e43a11b-3 ezoffT").text)
+    description = get_text_if_not_none(soup.find("p", class_="sc-2e43a11b-3 ezoffT"))
     print(description)
+
+    # récupérer tous les éléments p
+    div_elements = soup.find("div", class_="sc-1fac4d-0 guBswO")
+    e_elements = div_elements.find_all("p")
+    for e_element in e_elements:
+        print("ELEMENTS", e_element.text)
 
 else:
     print("ERREUR", response.status_code)
