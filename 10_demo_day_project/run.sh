ef351b90ba9507c12c24d5cb7223e011f2dd6a76
msche81 <mschenevier@gmail.com>
2025-01-28 10:38:35 +0100

    project

--- 10_demo_day_project/run.sh
+++ 10_demo_day_project/run.sh
@@ -3,8 +3,7 @@
 # Nom de l'image Docker
 IMAGE_NAME="skin_type_classifier"
 
-# Build de l'image Docker
-docker build -t $IMAGE_NAME .
-
 # Lancement du conteneur en mode interactif
-docker run -it --rm -p 7860:7860 $IMAGE_NAME
+docker run -it --rm -p 7860:7860 \
+    -v ~/.aws:/home/user/.aws:ro \
+    $IMAGE_NAME
